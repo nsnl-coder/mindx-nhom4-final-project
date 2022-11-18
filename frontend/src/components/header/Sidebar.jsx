0984ed40e04f3d1e0e149a307a2fcc4e13e43fd6
nsnl <nsnl.coder@gmail.com>
2022-11-18 06:59:10 -0500

    fix sidebar pug

--- frontend/src/components/header/Sidebar.jsx
+++ frontend/src/components/header/Sidebar.jsx
@@ -1,34 +1,34 @@
 import { Link } from 'react-router-dom'
 
-
 import searchIcon from '../../assets/icon-search.svg'
 import logo from '../../assets/logo-full.svg'
 
 const Sidebar = () => {
   return (
-    <div className="drawer-side">
-      <label htmlFor="my-drawer" className="drawer-overlay"></label>
-      <div className="w-80 h-screen bg-white px-4 py-4 flex flex-col">
-        <div className="flex-grow">
-          <Link to="/">
-            <img src={logo} alt="logo" className="h-8 mb-6" />
-          </Link>
-          <div className="flex w-full items-center gap-x-3">
-            <input
-              type="text"
-              className="border px-4 flex-grow h-9 rounded-full bg-gray-50 outline-none text-md text-gray-500 focus:shadow-sm"
-            />
-            <img src={searchIcon} alt="search" className="w-6 h-6 cursor-pointer" />
-          </div>
-        </div>
-        <div className="flex items-center space-x-2">
-          <img
-            src="https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/nancy_momoland_main_wm.png"
-            alt=""
-            className="w-9 aspect-square object-cover object-center rounded-full"
+    <div className="w-80 h-screen bg-white px-4 py-4 flex flex-col">
+      <div className="flex-grow">
+        <Link to="/">
+          <img src={logo} alt="logo" className="h-8 mb-6" />
+        </Link>
+        <div className="flex w-full items-center gap-x-3">
+          <input
+            type="text"
+            className="border px-4 flex-grow h-9 rounded-full bg-gray-50 outline-none text-md text-gray-500 focus:shadow-sm"
           />
-          <p className="font-semibold">Nancy Momoland</p>
+          <img
+            src={searchIcon}
+            alt="search"
+            className="w-6 h-6 cursor-pointer"
+          />
         </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <img
+          src="https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/nancy_momoland_main_wm.png"
+          alt=""
+          className="w-9 aspect-square object-cover object-center rounded-full"
+        />
+        <p className="font-semibold">Nancy Momoland</p>
       </div>
     </div>
   )
--- frontend/src/pages/newPost/NewPost.jsx
+++ frontend/src/pages/newPost/NewPost.jsx
@@ -1,10 +1,45 @@
 import { wrapperWithHeader } from '../../components'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import Editor from '../../components/editor/Editor'
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required()
 
 const NewPost = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <>
-      <h1>NewPost</h1>
-    </>
+    <div className="flex mt-8 max-w-6xl mx-auto px-10">
+      <div>
+        <input type="file" />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="border"
+          {...register('firstName')}
+          placeholder="Short description about your post"
+        />
+        <p>{errors.firstName?.message}</p>
+        <input className="border" {...register('age')} />
+        <p>{errors.age?.message}</p>
+
+        <Editor />
+        <input type="submit" />
+      </form>
+    </div>
   )
 }
 
