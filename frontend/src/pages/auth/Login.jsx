8242802cde073fb9ef1b96485dd7545c11a8cfd5
haolk003 <nguyenquochaolop91@gmail.com>
2022-11-18 15:47:36 +0700

    ✨ feat: login

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,9 +1,43 @@
+import Logo from '../../assets/logo-icon-big.svg';
 const Login = () => {
   return (
-    <>
-      <h1>Login</h1>
-    </>
-  )
-}
+    <div className="flex items-center justify-center h-screen text-black">
+      <div className="w-[550px] shadow-md shadow-gray rounded-md p-10 border-[1px] border-gray">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-3xl font-semibold text-black mb-5">Welcome back!</h1>
+            <p className="text-dark-gray">Good to see again.</p>
+          </div>
+          <img src={Logo} alt="" className="w-[150px] h-auto object-cover" />
+        </div>
+        <div>
+          <form>
+            <label className="text-lg font-semibold ">Email address:</label>
+            <br />
+            <input
+              type="email"
+              className="w-[80%] rounded-md h-12 px-4 shadow-sm shadow-gray border-[1px] border-gray my-2"
+            />
+            <br />
+            <label className="text-lg font-semibold">Password</label>
+            <br />
+            <input
+              type="password"
+              className="w-[80%] rounded-md h-12 px-4 shadow-sm shadow-gray border-[1px] border-gray my-2"
+            />
+            <br />
+            <button className="rounded-[50px] bg-primary w-[130px] my-4 text-white py-1 text-[17px] font-roboto font-semibold">
+              LOGIN
+            </button>
+            <p className="font-[600] text-[17px] mb-4">
+              Don't you have an account? <span className="text-primary text-lg cursor-pointer">Create new </span>now!
+            </p>
+            <p className="text-black font-semibold hover:underline cursor-pointer">Forgotten password?</p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
