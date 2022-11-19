8767326d67f90b2d2809f1a38e16d63fdd11db3e
haolk003 <nguyenquochaolop91@gmail.com>
2022-11-19 13:33:09 +0700

    🐞 fix(user):

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,40 +1,41 @@
-
-const mongoose=require('mongoose');
-const userSchema=mongoose.Schema({
-   
-    userName:{
-        type:String,
-        required:true,
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        require:true,
-        unique:true
+    email: {
+      type: String,
+      require: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        require:true,
+    password: {
+      type: String,
+      require: true,
     },
-    firstName:String,
-    lastName:String,
-    gender:String,
-    dateOfBirth:{
-        type:Date,
-        default:new Date()
+    firstName: String,
+    lastName: String,
+    gender: String,
+    dateOfBirth: {
+      type: Date,
+      default: new Date(),
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-    verified:{
-        type:Boolean,
-        default:false
+    verified: {
+      type: Boolean,
+      default: false,
     },
-    savedPosts:[{type:mongoose.Schema.Types.ObjectId,ref:"post"}],  
-    profileImage:{
-        type:String,
-        default:"https://dvdn247.net/wp-content/uploads/2020/07/avatar-mac-dinh-1.png"
-    }
-}, {timestamps:true}
+    savedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    profileImage: {
+      type: String,
+      default:
+        "https://dvdn247.net/wp-content/uploads/2020/07/avatar-mac-dinh-1.png",
+    },
+  },
+  { timestamps: true }
 );
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
