23a4fef7901be2e6fa8f030162f8659a709ac36d
haolk003 <nguyenquochaolop91@gmail.com>
2022-11-22 12:18:49 +0700

    🐞 fix:

--- api/src/routes/comment.js
+++ api/src/routes/comment.js
@@ -1,7 +1,7 @@
-const express=require('express');
-const {addComment}=require('../controllers/comment');
-const {verifyUser} =require('../utils/vertify');
-const router=express.Router();
-router.post('/:id',verifyUser,addComment);
+const express = require('express')
+const { addComment } = require('../controllers/comment')
+const { verifyUser, vertifyToken } = require('../utils/vertify')
+const router = express.Router()
+router.post('/:id', vertifyToken, addComment)
 
-module.exports=router;
+module.exports = router
