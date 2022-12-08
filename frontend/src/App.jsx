85eb2e5814403fda48e5c16b8b59986fdc9ec4c8
Minh Quan Do <minhquan98vn@gmail.com>
2022-12-08 22:47:31 +0700

    🌈 style: Adjust language button (again)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
             <BiGlobe />
           </label>
           <div className="collapse-content text-md rounded-xl bg-white">
-            <ul tabIndex={0} className="dropdown-content menu w-32 bg-base-100 shadow-md rounded-xl">
+            <ul tabIndex={0} className="dropdown-content menu w-32 bg-base-100 shadow-md rounded-xl top-28">
               <li data-name="en" className={`w-full p-2 hover:bg-gray-200 ${language === 'en' ? 'font-semibold' : ''} cursor-pointer`} onClick={onLanguageChange}>English</li>
               <li data-name="vi" className={`w-full p-2 hover:bg-gray-200 ${language === 'vi' ? 'font-semibold' : ''} cursor-pointer`} onClick={onLanguageChange}>Tiếng Việt</li>
             </ul>
